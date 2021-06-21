bbe4c74e02d41a22f9a2cf9b30be3a865a2d12ec
thangld2407 <thangld2407@gmail.com>
2021-06-21 17:01:22 +0700

    [FE][SECTION][fix section mask]

---
+++ assets/js/getTimeRemaining.js
@@ -0,0 +1,17 @@
+var timeRemaining = new Date("June 23, 2021 00:00:00").getTime()
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = timeRemaining - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("timeremaining").innerHTML = days + "d " + hours +"h " + minutes + "m " + seconds + "s ";
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("timeremaining").innerHTML = "EXPIRED";
+    }
+})
