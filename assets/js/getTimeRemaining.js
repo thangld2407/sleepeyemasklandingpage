74c15bbf5be482a743d6649d73df5e50fbc1ba07
thangld2407 <thangld2407@gmail.com>
2021-06-22 17:20:45 +0700

    [FE][SECTION][fix product section][DONE]

--- assets/js/getTimeRemaining.js
+++ assets/js/getTimeRemaining.js
@@ -9,9 +9,14 @@ var x = setInterval(function(){
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    document.getElementById("timeremaining").innerHTML = days + "d " + hours +"h " + minutes + "m " + seconds + "s ";
+    document.getElementById("timeremaining").innerHTML = hours +"h " + minutes + "m " + seconds + "s ";
     if(distance < 0){
         clearInterval(x);
         document.getElementById("timeremaining").innerHTML = "EXPIRED";
+    }
+    document.getElementById("time").innerHTML = hours +"h " + minutes + "m " + seconds + "s ";
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("time").innerHTML = "EXPIRED";
     }
 })
