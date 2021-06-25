8ff03ff06fe56f7f51f9f3d5b703d1a3f5d17598
thangld2407 <thangld2407@gmail.com>
2021-06-25 13:54:10 +0700

    [JS][Fix days of remainign]

--- assets/js/getTimeRemaining.js
+++ assets/js/getTimeRemaining.js
@@ -1,20 +1,21 @@
-var timeRemaining = new Date("June 29, 2021 00:00:00").getTime()
+var timeRemaining = new Date("June 26, 2021 00:00:00").getTime()
 
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = timeRemaining - now;
     
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + (days * 24);
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    document.getElementById("timeremaining").innerHTML = days + "d " + hours +"h " + minutes + "m " + seconds + "s ";
+    document.getElementById("timeremaining").innerHTML = hours +"h " + minutes + "m " + seconds + "s ";
     if(distance < 0){
         clearInterval(x);
         document.getElementById("timeremaining").innerHTML = "EXPIRED";
     }
-    document.getElementById("time").innerHTML = days + "d "+ hours +"h " + minutes + "m " + seconds + "s ";
+    document.getElementById("time").innerHTML = hours +"h " + minutes + "m " + seconds + "s ";
     if(distance < 0){
         clearInterval(x);
         document.getElementById("time").innerHTML = "EXPIRED";
